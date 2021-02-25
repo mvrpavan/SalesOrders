e05324e1fda24813b4e2e4c514d00d79082a9ca9
MVR Pavan Kumar <mvrpavan@gmail.com>
2021-02-25 09:31:45 +0530

    Login functionality is restored

--- SalesOrdersReport/Views/LoginForm.cs
+++ SalesOrdersReport/Views/LoginForm.cs
@@ -18,33 +18,36 @@ namespace SalesOrdersReport.Views
 
         private void btnLogin_Click(object sender, EventArgs e)
         {
-            //if (txtUserName.Text == "")
-            //{
-            //    MessageBox.Show("Please enter user name", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            //    txtUserName.Focus();
-            //    return;
-            //}
-            //if (txtPassword.Text == "")
-            //{
-            //    MessageBox.Show("Please enter password", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            //    txtPassword.Focus();
-            //    return;
-            //}
+#if DEBUG
+#else
+            if (txtUserName.Text == "")
+            {
+                MessageBox.Show(this, "Please enter user name", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txtUserName.Focus();
+                return;
+            }
+            if (txtPassword.Text == "")
+            {
+                MessageBox.Show(this, "Please enter password", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txtPassword.Focus();
+                return;
+            }
+#endif
             try
             {
                 MySqlConnection myConnection = MySQLHelper.GetMySqlHelperObj().GetDbConnection(); //CreateDBConnection();
                 if (myConnection == null) return;
 
-                //int ReturnVal = CommonFunctions.ObjUserMasterModel.LoginCheck(txtUserName.Text, txtPassword.Text, myConnection);
+#if DEBUG
                 int ReturnVal = 0;
                 MySQLHelper.GetMySqlHelperObj().CurrentUser = "admin";
+#else
+                int ReturnVal = CommonFunctions.ObjUserMasterModel.LoginCheck(txtUserName.Text, txtPassword.Text, myConnection);
+#endif
                 if (ReturnVal == 0)
                 {
                     CommonFunctions.CurrentUserName = MySQLHelper.GetMySqlHelperObj().CurrentUser;
                     //CommonFunctions.CurrentUserName = tmpIntegDBHelper.CurrentUser = "admin";
-
-                    //CommonFunctions.ObjUserMasterModel.LoadAllUserMasterTables();
-                    //CommonFunctions.ObjCustomerMasterModel.LoadAllCustomerMasterTables();
                     this.Close();
                     DialogResult = DialogResult.OK;
                 }
