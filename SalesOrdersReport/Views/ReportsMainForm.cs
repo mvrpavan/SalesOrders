239b1edc710084a1ebd54b0c3016b569a9c70b52
MVR Pavan Kumar <mvrpavan@gmail.com>
2021-04-26 12:49:05 +0530

    Build Version 8.0.4.0
    Build Version 8.0.4.0

--- SalesOrdersReport/Views/ReportsMainForm.cs
+++ SalesOrdersReport/Views/ReportsMainForm.cs
@@ -1,16 +1,9 @@
 ﻿using SalesOrdersReport.CommonModules;
 using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using SalesOrdersReport.Models;
-using SalesOrdersReport.CommonModules;
-using Excel = Microsoft.Office.Interop.Excel;
 using System.IO;
 
 namespace SalesOrdersReport.Views
